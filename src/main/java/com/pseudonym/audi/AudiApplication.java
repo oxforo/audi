9ce274b662e1caacca487a5623a8362f3a83b99e
oxforo <oxforo@naver.com>
2022-03-26 13:14:45 +0900

    SETTING : 초기 Spring 파일 생성

---
+++ src/main/java/com/pseudonym/audi/AudiApplication.java
@@ -0,0 +1,13 @@
+package com.pseudonym.audi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AudiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AudiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pseudonym/audi/AudiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pseudonym.audi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
